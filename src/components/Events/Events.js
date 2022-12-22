64fbf9762cdc98b88f66cfe3e38757405e7460c5
Elyes Arifa <earifa@resqsoft.com>
2022-12-22 02:07:11 +0100

    small fix

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import Event from './Event/Event';
-import useStyles from './styles';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import Event from "./Event/Event";
+import useStyles from "./styles";
+import { useNavigate } from "react-router-dom";
 
-import { Grid, CircularProgress, Button } from '@material-ui/core';
-import { useSelector } from 'react-redux';
-import AddIcon from '@mui/icons-material/Add';
+import { Grid, CircularProgress, Button } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import AddIcon from "@mui/icons-material/Add";
 
 const Events = ({ setCurrentId, searchData }) => {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
-  const { events, isLoading } = useSelector((state) => state.events);
+  const { events, isLoading } = useSelector((state) => state?.events);
   const navigate = useNavigate();
 
   const nav = () => {
     if (user) {
-      navigate('/createEvent');
+      navigate("/createEvent");
     } else {
-      navigate('/auth');
+      navigate("/auth");
     }
   };
 
@@ -26,7 +26,10 @@ const Events = ({ setCurrentId, searchData }) => {
   if (!events.length && !isLoading)
     return (
       <div>
-        <div>No current events in your area, be the first one to organize something of a value! </div>
+        <div>
+          No current events in your area, be the first one to organize something
+          of a value!{" "}
+        </div>
         <Button onClick={nav} fontSize="small" edge="false">
           New Event
           <AddIcon fontSize="large"></AddIcon>
@@ -37,7 +40,12 @@ const Events = ({ setCurrentId, searchData }) => {
   return isLoading ? (
     <CircularProgress />
   ) : (
-    <Grid className={classes.container} container alignItems="stretch" spacing={2}>
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={2}
+    >
       {events.map((event) => (
         <Grid key={event._id} item xs={12} sm={12} md={6} lg={4}>
           <Event event={event} setCurrentId={setCurrentId} />
