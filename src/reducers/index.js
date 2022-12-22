4adfdfc194e588b06cecc09ecf7d3ad298d22b89
Elyes Arifa <earifa@resqsoft.com>
2022-12-22 01:50:17 +0100

    cached issue

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import events from './events';
-import auth from './auth';
-import position from './position';
-
-export default combineReducers({ events, auth, position });
